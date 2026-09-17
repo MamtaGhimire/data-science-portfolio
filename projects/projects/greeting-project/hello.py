3d57fd0e46f5322821a81f32081f16bf098d6965
MamtaGhimire <mamtaghimire05@gmail.com>
2026-09-17 11:57:56 -0700

    Add career goal question

--- projects/projects/greeting-project/hello.py
+++ projects/projects/greeting-project/hello.py
@@ -3,9 +3,11 @@
 def greet_user():
     name = input("Enter your name: ")
     favorite_subject = input("What is your favorite subject? ")
+    career_goal = input("What career are you interested in? ")
 
     print(f"Hello, {name}! Welcome to Data Science.")
     print(f"It is awesome that you like {favorite_subject}.")
+    print(f"Data Science can help you work toward becoming a {career_goal}.")
 
 if __name__ == "__main__":
     greet_user()
