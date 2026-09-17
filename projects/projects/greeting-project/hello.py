b4a78f03d63145d15247fcdb09ab7a5c64ede553
MamtaGhimire <mamtaghimire05@gmail.com>
2026-09-17 11:56:32 -0700

    Add favorite subject question

--- projects/projects/greeting-project/hello.py
+++ projects/projects/greeting-project/hello.py
@@ -2,7 +2,10 @@
 
 def greet_user():
     name = input("Enter your name: ")
+    favorite_subject = input("What is your favorite subject? ")
+
     print(f"Hello, {name}! Welcome to Data Science.")
+    print(f"It is awesome that you like {favorite_subject}.")
 
 if __name__ == "__main__":
     greet_user()
